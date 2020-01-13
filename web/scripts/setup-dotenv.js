5a8a6dd9b7609dc8bea90a3e89375c01a4661e1a
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-01-13 16:56:51 +0200

    fix path on setup-dotenv

--- web/scripts/setup-dotenv.js
+++ web/scripts/setup-dotenv.js
@@ -31,7 +31,7 @@ if (!environment) {
 }
 
 dotenv.config({
-  path: path.resolve(`../config/.env.${environment}`),
+  path: path.resolve(`web/config/.env.${environment}`),
 });
 
 if (otherArgs.length) {
