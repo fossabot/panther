e92cfdfee37530b72a593ec75233392dbfeeac2a
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-01-13 12:40:04 +0200

    add enterprise file replacement plugin

---
+++ web/dev-utils/dir-content-replacement-plugin.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2020 Panther Labs Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* eslint-disable no-param-reassign */
+const fs = require('fs');
+const klawSync = require('klaw-sync');
+
+class PantherEnterpriseReplacementPlugin {
+  constructor({ dir, mapper }) {
+    this.dir = dir;
+    this.mapper = mapper;
+  }
+
+  apply(compiler) {
+    if (!fs.existsSync(this.dir)) {
+      return;
+    }
+
+    const filePaths = klawSync(this.dir, {
+      nodir: true,
+      traverseAll: true,
+    }).map(f => f.path);
+
+    const originToDestinationFilePathMapping = {};
+    filePaths.forEach(filePath => {
+      originToDestinationFilePathMapping[this.mapper(filePath)] = filePath;
+    });
+    const originAbsFilePaths = Object.keys(originToDestinationFilePathMapping);
+
+    compiler.hooks.normalModuleFactory.tap('PantherEnterpriseReplacementPlugin', nmf => {
+      nmf.hooks.afterResolve.tap('PantherEnterpriseReplacementPlugin', result => {
+        if (!result) {
+          return undefined;
+        }
+
+        if (originAbsFilePaths.includes(result.resource)) {
+          result.resource = originToDestinationFilePathMapping[result.resource];
+        }
+
+        return result;
+      });
+    });
+  }
+}
+
+module.exports = PantherEnterpriseReplacementPlugin;
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2020 Panther Labs Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const path = require('path');
 const resolve = require('resolve');
 const webpack = require('webpack');
@@ -10,6 +26,10 @@ const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const ReactRefreshPlugin = require('react-refresh-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const DirContentReplacementPlugin = require('./dev-utils/dir-content-replacement-plugin');
+
+// Treating root project dir as CWD since this scripts is called through npm
+const ENTERPRISE_DIR_NAME = 'enterprise';
 
 const isEnvDevelopment = process.env.NODE_ENV === 'development';
 const isEnvProduction = process.env.NODE_ENV === 'production';
@@ -293,6 +313,10 @@ module.exports = {
       ],
       watch: path.resolve(__dirname, 'src'),
       silent: true,
+    }),
+    new DirContentReplacementPlugin({
+      dir: path.resolve(__dirname, `../${ENTERPRISE_DIR_NAME}/web`),
+      mapper: filePath => filePath.replace(`/${ENTERPRISE_DIR_NAME}/`, `/`),
     }),
   ].filter(Boolean),
 };
